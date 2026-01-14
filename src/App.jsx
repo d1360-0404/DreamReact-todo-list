130e9aad47f31b8877d1745e418798483491cfaa
Diego Mendoza <67606427+d1360-0404@users.noreply.github.com>
2026-01-14 00:32:27 -0600

    Load Todos from airtble
    Establis a transition state. Allowed airtable to be fetched in app and display loading indicator in todoList. Also display any error that occur.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,51 @@
 import './App.css'
 import TodoList from "./features/TodoList/TodoList.jsx";
 import TodoForm  from "./features/TodoForm.jsx";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [todoList, setTodoList]=useState([]);
+  const [isLoading,setLoading]=useState(false);
+  const [errorMessage,setErrorMessage]=useState(""); 
+  const url = `https://api.airtable.com/v0/${import.meta.env.VITE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+  const token = `Bearer ${import.meta.env.VITE_PAT}`;
+
+  useEffect(()=>{
+    const fetchTodos = async () => {
+      setLoading(true);
+      const options={
+        method:"GET",
+        headers:{
+          Authorization: token,
+        },
+      };
+      try {
+          const resp=await fetch(url,options);
+          if(!resp.ok){
+            throw new Error(resp.message);
+          }
+          const {records}=await resp.json();
+          setTodoList(records.map((record)=>{
+            const example={
+              id:record.id,
+              ...record.fields,
+            }
+            if(!example.booleanProperty){
+              example.booleanProperty=false;
+            }
+            return example;
+          }));
+          
+        } 
+        catch (error) {
+          setErrorMessage(error.message);
+      }finally{
+        setLoading(false);
+      }
+    };
+    fetchTodos();
+  
+  },[]);
 
   function addTodo(title){
     const newTodo={
@@ -40,7 +81,19 @@ function App() {
     <div>
       <h1>My Todos</h1>
       <TodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList} onCompleteTodo={completeTodo} onUpdateTodo={updateTodo} />
+      <TodoList todoList={todoList} onCompleteTodo={completeTodo} onUpdateTodo={updateTodo} isLoading={isLoading}/>
+      {errorMessage !=""? (
+        <div>
+          <hr />
+          <p>{errorMessage}</p>
+          <button onClick={()=>{
+            setErrorMessage('');
+          }}>Dismiss</button>
+        </div>
+        ) : (
+          <React.Fragment></React.Fragment>
+      )
+      };
       </div>
   )
 }
