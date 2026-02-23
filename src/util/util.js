3b0088040982ca86de0cbfc3ad499f56161640ba
Diego Mendoza <67606427+d1360-0404@users.noreply.github.com>
2026-02-23 01:24:35 -0600

    Update util.js

--- src/util/util.js
+++ src/util/util.js
@@ -1,11 +1,28 @@
 export async function sendResquest(url,options){
-  const resp=await fetch(url,options);
-  
-  if(!resp.ok){
-    const errorMSG=await resp.json();
-    console.log(errorMSG.error);
-    throw new Error(errorMSG?.error?.message);
-  }
+  const errorMessage="Unknown error"
 
+  try {
+    const resp=await fetch(url,options);
+    if(!resp.ok){
+      let errorMSG=null;
+      try{
+        //second try if await resp.json() fails
+        errorMSG=await resp.json();
+      }
+      catch{
+      }
+      throw new Error(
+        //catches each possible edge case
+        errorMSG?.error?.message ||
+        errorMSG?.error ||
+        resp.statusText ||
+        errorMessage
+      );
+    }
     return resp;
+  } catch (error) {
+    throw new Error(`Error: ${error.message}`);
+  }
 }
+
+
